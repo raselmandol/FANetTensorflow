f5c85bb367920237770b019974fe4a8a1e3ab5e7
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-12-13 22:36:04 +0530

    Update lossUsingAPI.py

--- lossUsingAPI.py
+++ lossUsingAPI.py
@@ -5,7 +5,7 @@ from tensorflow.keras import backend as K
 def dice_coefficient(y_true, y_pred, smooth=1):
     y_pred = K.sigmoid(y_pred)
     y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred) #y_pred
+    y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
     dice = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
     return dice
